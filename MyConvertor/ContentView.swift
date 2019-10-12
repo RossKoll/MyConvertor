f89c5de04e2af1df0b4a6160c861ff2e0b54fa23
Ross <rosko48@gmail.com>
2019-10-12 22:40:39 +0300

    Initial Commit

---
+++ MyConvertor/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyConvertor
+//
+//  Created by Ross on 10/12/19.
+//  Copyright © 2019 Ross. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
