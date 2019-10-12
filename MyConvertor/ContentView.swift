511641a409e6b6047cb36b0c4563de4b5ab01d4a
Ross <rosko48@gmail.com>
2019-10-12 22:45:19 +0300

    first commit

--- MyConvertor/ContentView.swift
+++ MyConvertor/ContentView.swift
@@ -1,6 +1,6 @@
 //
 //  ContentView.swift
-//  MyConvertor
+//  Convert
 //
 //  Created by Ross on 10/12/19.
 //  Copyright © 2019 Ross. All rights reserved.
@@ -9,8 +9,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var inputValue = ""
+    @State private var inputUnit = 1
+    @State private var outputUnit = 1
+    
+    let inputUnits = ["Seconds", "Minutes", "Hours", "Days"]
+    let outputUnits = ["Seconds", "Minutes", "Hours", "Days"]
+    
+    var inValue: Double{
+        let inpValue = Double(inputValue) ?? 0
+        let result = inpValue * 60
+        
+        return result
+    }
+    
     var body: some View {
-        Text("Hello World")
+        NavigationView{
+        Form{
+            Section{
+                TextField("Input value", text: $inputValue)
+            }
+            
+            Section(header: Text("Initial Unit")){
+                Picker("Initial unit", selection: $inputUnit){
+                    ForEach(0 ..< inputUnits.count){
+                        Text("\(self.inputUnits[$0])")
+                    }
+                }.pickerStyle(SegmentedPickerStyle())
+            }
+            
+            Section(header: Text("Output Unit")){
+                Picker("Output unit", selection: $outputUnit){
+                    ForEach(0 ..< outputUnits.count){
+                        Text("\(self.outputUnits[$0])")
+                    }
+                }.pickerStyle(SegmentedPickerStyle())
+            }
+            
+            Section(header: Text("Converted value")){
+                Text("\(inValue)")
+            }
+            }.navigationBarTitle("MyConverter")
+        }
     }
 }
 
